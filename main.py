9ad92e2d257f01b0cc260da4ec9dd5a184dd1407
Ching Yi, Chan <qrtt1@infuseai.io>
2023-05-05 15:50:57 +0800

    Init
    Signed-off-by: Ching Yi, Chan <qrtt1@infuseai.io>

---
+++ main.py
@@ -0,0 +1,20 @@
+from typing import Union
+
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=5000, log_level="info")
